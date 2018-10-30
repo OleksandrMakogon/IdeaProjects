96714942feebee8e741343f380504c1dfcdab952
Oleksandr Makogon <wicked2007@ukr.net>
2018-10-30 16:47:26 +0200

    unitTests

--- sports-betting-application/src/test/java/TestHashCalculator.java
+++ sports-betting-application/src/test/java/TestHashCalculator.java
@@ -7,17 +7,17 @@ public class TestHashCalculator {
     HashCalculator hashCalc = new HashCalculator();
 
     @Test
-    public void Test1(){
+    public void StringToHash_StringInput(){
         String actual = hashCalc.hash("Hello");
         String expected = "8b1a9953c4611296a827abf8c47804d7";
         Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void Test2(){
-        String actual = hashCalc.hash("Hello");
-        String expected = "04063d1d97b3dc99595786095f62a14c";
-        Assert.assertNotEquals(expected, actual);
+    public void StringToHash_IntegerInput(){
+        String actual = hashCalc.hash("1213");
+        String expected = "33ceb07bf4eeb3da587e268d663aba1a";
+        Assert.assertEquals(expected, actual);
     }
 
 
